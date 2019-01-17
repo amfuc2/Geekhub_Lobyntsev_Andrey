a028add3d562fc65c9e02ea31ccc00228b993d47
amfuc2 <45484477+amfuc2@users.noreply.github.com>
2019-01-17 16:27:54 +0200

    Home14 array

---
+++ index.php
@@ -0,0 +1,232 @@
+<?php
+
+return [
+    'startScreen' => [
+        'siteLogo' => 'src/img/Logo.png',
+        'mainMenu' => [
+            [
+                'title' => 'Home',
+                'url' => '#',
+            ],
+            [
+                'title' => 'About us',
+                'url' => '#',
+            ],
+            [
+                'title' => 'Offer',
+                'url' => '#',
+            ],
+            [
+                'title' => 'Portfolio',
+                'url' => '#',
+            ],
+            [
+                'title' => 'Contact',
+                'url' => '#',
+            ],
+        ],
+        'generalText' => [
+            'mainTitle' => [
+                'title' => 'Lorem ipsum dolor sit amet',
+            ],
+            'mainSubtitle' => [
+                'title' => 'Nunc vel nibh tempor, pharetra lectus congue, luctus orci.',
+            ],
+            'buttonPriceList' => [
+                'title' => 'Ask for price',
+                'url' => '#',
+            ],
+        ],
+        'hint' => [
+            'hintIcon' => 'src/img/Rounded_Rectangle_2.png',
+            'hintText' => [
+                'title' => 'scroll down',
+            ],
+        ],
+    ],
+    'merits' => [
+        'BestDesign' => [
+            'mainTitle' => [
+                'title' => 'BestDesign',
+            ],
+            'subjectText' => [
+                'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur 
+                ullamcorper auctor mauris, placerat fermentum lectus vulputate sed. Phasellus vestibulum sit amet dolor
+                eget eleifend.',
+            ],
+            'buttonPriceList' => [
+                'title' => 'Ask for price',
+                'url' => '#',
+            ],
+        ],
+        'BestCode' => [
+            'mainTitle' => [
+                'title' => 'BestCode',
+            ],
+            'subjectText' => [
+                'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur 
+                ullamcorper auctor mauris, placerat fermentum lectus vulputate sed. Phasellus vestibulum sit amet dolor
+                eget eleifend.',
+            ],
+            'buttonPriceList' => [
+                'title' => 'Ask for price',
+                'url' => '#',
+            ],
+        ],
+    ],
+    'employment' => [
+        'generalTitle' => [
+            'title' => 'What we do?',
+        ],
+        'sections of employment' => [
+            'Web design' => [
+                'mainTitle' => [
+                    'title' => 'Web design',
+                ],
+                'subjectText' => [
+                    'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                     Curabitur ullamcorper auctor mauris, placerat fermentum.',
+                ],
+            ],
+            'Web application' => [
+                'mainTitle' => [
+                    'title' => 'Web application',
+                ],
+                'subjectText' => [
+                    'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                     Curabitur ullamcorper auctor mauris, placerat fermentum.',
+                ],
+            ],
+            'Digital painting' => [
+                'mainTitle' => [
+                    'title' => 'Digital painting',
+                ],
+                'subjectText' => [
+                    'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                     Curabitur ullamcorper auctor mauris, placerat fermentum.',
+                ],
+            ],
+            'Desktop application' => [
+                'mainTitle' => [
+                    'title' => 'Desktop application',
+                ],
+                'subjectText' => [
+                    'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                     Curabitur ullamcorper auctor mauris, placerat fermentum.',
+                ],
+            ],
+        ],
+        'buttonPriceList' => [
+            'title' => 'Ask for price',
+            'url' => '#',
+        ],
+    ],
+    'ourTemplates' => [
+        'DroneZone Template' => [
+            'templateImage' => 'src/img/Layer_12.png',
+            'overlay' => [
+                'templateName' => [
+                    'title' => 'DroneZone Template',
+                ],
+                'subjectText' => [
+                    'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur
+                    ullamcorper auctor mauris, placerat fermentum.',
+                ],
+                'buttonMore' => [
+                    'title' => 'Watch more',
+                    'url' => '#',
+                ],
+            ],
+        ],
+        'Neptune Template' => [
+            'templateImage' => 'src/img/Layer_14.png',
+            'overlay' => [
+                'templateName' => [
+                    'title' => 'Neptune Template',
+                ],
+                'subjectText' => [
+                    'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur
+                    ullamcorper auctor mauris, placerat fermentum.',
+                ],
+                'buttonMore' => [
+                    'title' => 'Watch more',
+                    'url' => '#',
+                ],
+            ],
+        ],
+        'FitApp Template' => [
+            'templateImage' => 'src/img/Layer_13.png',
+            'overlay' => [
+                'templateName' => [
+                    'title' => 'FitApp Template',
+                ],
+                'subjectText' => [
+                    'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur
+                    ullamcorper auctor mauris, placerat fermentum.',
+                ],
+                'buttonMore' => [
+                    'title' => 'Watch more',
+                    'url' => '#',
+                ],
+            ],
+        ],
+    ],
+    'information about the team' => [
+        'generalTitle' => [
+            'title' => 'About us!',
+        ],
+        'about the team' => [
+            'title' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque. Curabitur ullam-
+                corper auctor mauris, placerat fermentum.',
+        ],
+        'about a team member' => [
+            'person1' => [
+                'personPhoto' => 'src/img/Layer_16.png',
+                'name' => [
+                    'title' => 'John Doe',
+                ],
+                'aboutPerson' => [
+                    'title' => 'Curabitur ullamcorper auctor mauris, placerat fermentum.',
+                ],
+            ],
+            'person2' => [
+                'personPhoto' => 'src/img/Layer_17.png',
+                'name' => [
+                    'title' => 'John Doe',
+                ],
+                'aboutPerson' => [
+                    'title' => 'Curabitur ullamcorper auctor mauris, placerat fermentum.',
+                ],
+            ],
+            'person3' => [
+                'personPhoto' => 'src/img/Layer_18.png',
+                'name' => [
+                    'title' => 'John Doe',
+                ],
+                'aboutPerson' => [
+                    'title' => 'Curabitur ullamcorper auctor mauris, placerat fermentum.',
+                ],
+            ],
+            'person4' => [
+                'personPhoto' => 'src/img/Layer_4_copy_3.png',
+                'name' => [
+                    'title' => 'John Doe',
+                ],
+                'aboutPerson' => [
+                    'title' => 'Curabitur ullamcorper auctor mauris, placerat fermentum.',
+                ],
+            ],
+        ],
+    ],
+    'map' => 'src/img/Ultra_Light_with_Labels___Snazzy_Maps___Free_Styles_for_Google_Maps.png',
+    'footer' => [
+        [
+            'title' => 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        ],
+        [
+            'title' => 'Try awsome tool for desgners symu.co',
+        ],
+    ],
+];
+
+?>
